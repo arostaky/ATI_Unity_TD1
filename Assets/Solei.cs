64f427a592307fd2117571bc287b4503dd688454
Patricio <jpdibacco@gmail.com>
2020-10-20 20:46:34 +0200

    made the planets rotate

--- Assets/Solei.cs
+++ Assets/Solei.cs
@@ -5,15 +5,9 @@ using UnityEngine;
 public class Solei : MonoBehaviour{
     public float vitesseTournerAutour;
     public Transform centre;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.RotateAround(centre.position, Vector3.up, vitesseTournerAutour * Time.deltaTime);
     }
 }
