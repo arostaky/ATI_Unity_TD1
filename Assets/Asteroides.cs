aafdf49d5abc79e1bb30b1fe8317eefa4b1b16dd
Patricio <jpdibacco@gmail.com>
2020-10-20 21:21:43 +0200

    added asteriod script

---
+++ Assets/Asteroides.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroides : MonoBehaviour{
+    private Vector3 axe = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        axe.x = Random.Range(-90,90);
+        axe.y = Random.Range(-90,90);
+        axe.z = Random.Range(-90,90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        transform.Rotate(axe * Time.deltaTime);
+    }
+}
---
+++ Assets/Vaisseau.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaisseau : MonoBehaviour{
+    public Transform planete1;
+    public float vitesse;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, planete1.position, Time.deltaTime * vitesse);
+
+    }
+}
